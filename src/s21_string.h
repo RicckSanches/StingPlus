8b7f3815fd4208708d380f225d367d2eed29f2ae
ralif <yessir339@mail.ru>
2025-12-15 18:33:10 +0300

    s21_sprintf() added

--- src/s21_string.h
+++ src/s21_string.h
@@ -5,11 +5,13 @@
 #define s21_size_t unsigned long long
 #define s21_NULL (void*)0
 
-int s21_strlen(char *str);
+s21_size_t s21_strlen(char *str);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
+
+int s21_sprintf(char *str, const char *format, ...);
 
 #endif
