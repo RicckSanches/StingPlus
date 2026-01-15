09cc1f98d26e9de62e1dc97546ecebb68a036172
weeperti <glebasanxv@yandex.ru>
2026-01-15 15:05:14 +0700

    .

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 int s21_sprintf(char* buff, const char* format, ...);
+int int_to_char(int value, char* str);
 
 int main(){
 char buffer[100];
 char* str = "world";
-s21_sprintf(buffer, "Hello, %s!", str);
+s21_sprintf(buffer, "Hello, %d!", -10);
 printf("%s", buffer);
 }
 
@@ -25,8 +26,10 @@ int s21_sprintf(char* buff, const char* format, ...){
             }
             if(*format == 'd'){
                 int x = va_arg(ap, int);
-
+                int len = int_to_char(x, out);
+                out+= len;
             }
+            format++;
         }
         *out++ = *format++; 
     }
@@ -36,3 +39,32 @@ int s21_sprintf(char* buff, const char* format, ...){
 
 }
 
+int int_to_char(int value, char* str){
+    char buffer[32];
+    int number = 0;
+    int negative = 0;
+    int i = 0;
+    if(value == 0){
+        buffer[0] = 0 + '0';
+    }
+    else{
+    if(value < 0){
+        negative = 1;
+        value = -value;
+    }
+    while(value != 0){
+        number = value % 10;
+        buffer[i] = number + '0';
+        value /= 10;
+        i++;
+    }
+    if(negative){
+        *str++ = '-';
+    }
+    for(int j = i - 1; j >= 0; j--){
+        *str++ = buffer[j];
+    }
+    *str = '\0';
+}
+return negative ? i + 1 : i;
+}
